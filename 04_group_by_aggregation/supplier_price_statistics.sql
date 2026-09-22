0e6625c1265bb6a6f7691d3e71f86ccf74beb717
Krystyna Ahieieva <151083778+Ammarri@users.noreply.github.com>
2026-09-22 21:27:12 +0300

    Update supplier_price_statistics.sql

--- 04_group_by_aggregation/supplier_price_statistics.sql
+++ 04_group_by_aggregation/supplier_price_statistics.sql
@@ -6,16 +6,8 @@ SELECT
 	SupplierID,
 	MIN(UnitPrice) AS min_price,
 	MAX(UnitPrice) AS max_price,
-	CAST(AVG(UnitPrice)AS DECIMAL(10,2)) AS avg_price,   --до двох знаків після коми, незаповнені ціни не рахую
-	SUM(UnitPrice * ISNULL(UnitsInStock,0)) AS total_value --захист через мінімум один ISNULL обов'язковий,коли є множення Nullable стовпців  
+	CAST(AVG(UnitPrice)AS DECIMAL(10,2)) AS avg_price,   --до двох знаків після коми, незаповнені ціни не рахую 
+	SUM(UnitPrice * UnitsInStock) AS total_value 
 FROM Products
 GROUP BY SupplierID
 ORDER BY AVG(UnitPrice) DESC;
-
--- Доп запит: перевірка на кількість NULL 
-SELECT 
-	ProductID,
-	UnitPrice,
-	UnitsInStock
-FROM Products
-WHERE UnitPrice IS NULL OR UnitsInStock IS NULL;
